24425b6bc886105f6fcc2140fbf401d94eecd999
Ashok Sah <ashok.stelmec@gmail.com>
2022-07-16 21:20:20 +0530

    Limited the Textarea Element not greater than Thousands letters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Ajay</h2>
+      <textarea maxlength="1000" name="w3review" rows="4" cols="50" onChange={handleChange} />
+      <p>{text.length}/1000</p>
+      
     </div>
   );
 }
